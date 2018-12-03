5d9f127f4c81c3f63289b92846ff3df744eb5b52
Rohan Bansal <rohan@digithinkit.com>
2018-12-03 12:07:26 +0530

    fix(webform): Fix path to New button

--- repairs/repairs/web_form/service_and_repair/service_and_repair.js
+++ repairs/repairs/web_form/service_and_repair/service_and_repair.js
@@ -1,5 +1,5 @@
 // Dirty hack to remap the "New" button on the portal, since
 // there is no framework provision to do so
 frappe.ready(function () {
-	$(".page-header-actions-block a.btn-new").attr("href", "/fit_correction");
+	$(".page-header-actions-block a.btn-new").attr("href", "/fit");
 })
