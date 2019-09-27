a6751b7705ff7f47ac6fe45779c80fc1762fd08d
Rohan Bansal <rohan@digithinkit.com>
2019-09-27 17:38:55 +0530

    fix: also refer old impression IDs for iem owner search

--- repairs/utils.py
+++ repairs/utils.py
@@ -79,13 +79,18 @@ def set_iem_owner(warranty_claim, method):
 
 			if impression_id:
 				if frappe.db.exists("Serial No", serial_no):
-					iem_owner = frappe.db.get_value("IEM Owner", {"impression_id": impression_id}, "name")
-
 					frappe.db.set_value("Serial No", serial_no, "impression_id", impression_id)
-					frappe.db.set_value("Serial No", serial_no, "iem_owner", iem_owner)
+
+					iem_owner = frappe.get_all("IEM Owner", or_filters={"impression_id": impression_id, "old_impression_id": impression_id})
+					if iem_owner:
+						frappe.db.set_value("Serial No", serial_no, "iem_owner", iem_owner[0].name)
 
 		if impression_id:
-			warranty_claim.iem_owner = frappe.db.get_value("IEM Owner", {"impression_id": impression_id}, "name")
+			iem_owner = frappe.get_all("IEM Owner", or_filters={"impression_id": impression_id, "old_impression_id": impression_id})
+			if iem_owner:
+				warranty_claim.iem_owner = iem_owner[0].name
+			else:
+				warranty_claim.iem_owner = None
 		else:
 			warranty_claim.iem_owner = None
 
